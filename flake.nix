23712c2c8ae4b5af14435058675ea45762ccd615
jacob whitford-bender <jacob@xmentium.com>
2025-01-08 23:38:00 -0700

    feat: works for serious eats. what more could one need?

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    { nixpkgs, self, flake-utils, ... }: flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin"] (system:
+    let pkgs = nixpkgs.legacyPackages.${system}; in
+    {
+       devShells.default = pkgs.mkShell {
+         packages = with pkgs; [gnumake nodejs];
+         shellHook = ''exec zsh'';
+       };
+       packages.default = pkgs.buildNpmPackage {
+         dontNpmBuild = true;
+         name = "extractor";
+         npmDepsHash = "sha256-8XqJRqiThHQb1YnX8vqurJ6VqHH/3fCagnv48AAh2ro=";
+         src = ./.;
+       };
+       apps.default = {
+         type = "app";
+         program = "${self.packages."${system}".default}/lib/node_modules/json-ld-extractor/main.js";
+       };
+    });
+}
---
+++ main.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+import * as htmlparser2 from 'htmlparser2'
+import {Readable} from 'node:stream'
+import {parseArgs} from 'node:util'
+import {URL} from 'node:url'
+
+const randomId = () => globalThis.crypto.randomUUID()
+
+const {positionals: [input]} = parseArgs({allowPositionals: true})
+if (!URL.canParse(input)) {
+  throw new Error('your input is broken. please feed me a url')
+}
+
+const main = async input => {
+  let inTag = undefined
+  const texts = new Map()
+  const parser = new htmlparser2.Parser({
+      onopentag(name, attributes) {
+          const {id = randomId(), type} = attributes
+          if (name === "script" && type === "application/ld+json") {
+            inTag = id
+            texts.set(id, [])
+          }
+      },
+      ontext(text) {
+        if (inTag) texts.get(inTag).push(text)
+      },
+      onclosetag(name) {
+          if (name === "script" && inTag) {
+            const value = texts.get(inTag)
+            try {
+              const json = JSON.parse(value.join(''))
+              const ary = Array.isArray(json) ? json : [json]
+              const recipe = ary.find (o => o['@type']?.includes('Recipe'))
+              console.log(JSON.stringify(recipe))
+            } catch (e) {
+              console.error(e)
+            }
+            texts.delete(inTag)
+            inTag = undefined
+          }
+      }
+  })
+
+  const source = await fetch(input)
+  const sourceStream = Readable.fromWeb(source.body)
+  sourceStream.on('data', chunk => {
+    parser.write(chunk.toString())
+  })
+
+  return new Promise((res, rej) => {
+    sourceStream.on('error', rej)
+    sourceStream.on('end', () => {
+      parser.end()
+      res()
+    })
+  })
+}
+
+await main(input)
